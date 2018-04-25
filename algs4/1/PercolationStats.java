13dc95fcbe29e8166174e46a54b3ca537079bc3e
crkylin <crkylin@gmail.com>
2018-04-25 21:43:16 +0800

    n

---
+++ algs4/1/PercolationStats.java
@@ -0,0 +1,25 @@
+public class PercolationStats {
+    public PercolationStats(int n, int trials) {
+
+    }
+
+    public double mean() {
+
+    }
+
+    public double stddev() {
+
+    }
+
+    public double confidenceLo() {
+
+    }
+
+    public double condifenceHi() {
+
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
